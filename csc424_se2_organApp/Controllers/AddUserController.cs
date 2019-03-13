01c152821acb4e8cae5ba6b0b9e5bb3988e03949
brandonburchett <brandonburchett@gmail.com>
2019-03-13 16:59:45 -0500

    registered DB context with backend (I think) and setting up post route to add user

---
+++ csc424_se2_organApp/Controllers/AddUserController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BCrypt.Net;
+using csc424_se2_organApp.Models;
+
+namespace csc424_se2_organApp.Controllers
+{
+    using BCrypt = BCrypt.Net.BCrypt;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddUserController : Controller{
+        private readonly organ_appContext _context;
+    
+        public AddUserController(organ_appContext context)         
+        {             
+            _context = context;       
+        }
+        [HttpPost]
+        public JsonResult NewUser([FromBody]Users user){
+            
+            user.Password = BCrypt.HashPassword(user.Password);
+            _context.Add(user);
+            return Json(user);
+        
+        }
+    }
+
+}
