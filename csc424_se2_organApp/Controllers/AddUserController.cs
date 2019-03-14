01ef214b1c247739ee1a53d0d46e34494f719534
brandonburchett <brandonburchett@gmail.com>
2019-03-14 14:16:10 -0500

    functional signup page

--- csc424_se2_organApp/Controllers/AddUserController.cs
+++ csc424_se2_organApp/Controllers/AddUserController.cs
@@ -10,25 +10,38 @@ using csc424_se2_organApp.Models;
 namespace csc424_se2_organApp.Controllers
 {
     using BCrypt = BCrypt.Net.BCrypt;
+    
     [Route("api/[controller]")]
     [ApiController]
-    
     public class AddUserController : Controller{
-        private readonly organ_appContext _context;
+
+        private static ActionResult Result(int statusCode, string reason) => new ContentResult
+        {
+            StatusCode = statusCode,
+            Content = $"Status Code: {(int)statusCode}; {statusCode}; {reason}",
+            ContentType = "text/plain"
+        };
+        private readonly organ_appContext context;
     
-        public AddUserController(organ_appContext context)         
+        public AddUserController(organ_appContext _context)         
         {             
-            _context = context;       
+            context = _context;       
         }
         
         
         [HttpPost]
         public JsonResult NewUser([FromBody]Users user){
             
+            var isInDb = context.Users.Find(user.Email);
+            if(isInDb != null){
+                Response.StatusCode = 422;
+                return Json(new {error="User Already Exists"}); 
+            }
             user.Password = BCrypt.HashPassword(user.Password);
-            _context.Users.Add(user);
-            _context.SaveChanges();
-            return Json(user);
+            context.Users.Add(user);
+            context.SaveChanges(); 
+            Response.StatusCode = 201;
+            return Json(new {message = $"{user.Email} created"});
         
         }
     }
