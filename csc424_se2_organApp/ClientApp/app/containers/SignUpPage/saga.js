9d6455efb13e1d274a3c6925807a56cbb0335517
brandonburchett <brandonburchett@gmail.com>
2019-03-14 14:16:10 -0500

    functional signup page

--- csc424_se2_organApp/ClientApp/app/containers/SignUpPage/saga.js
+++ csc424_se2_organApp/ClientApp/app/containers/SignUpPage/saga.js
@@ -15,7 +15,8 @@ export function* addUser() {
       'Content-Type': 'application/json',
     }
     axios.post("http://localhost:5000/api/AddUser",body,headers)
-    .then(res => console.log(res));
+    .then(res => console.log(res))
+    .catch(err=>alert(err.response.data.error));
   } catch (err) {
     alert(err);
   }
